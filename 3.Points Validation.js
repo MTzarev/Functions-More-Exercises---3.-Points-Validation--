f9ae0922f28b2673c7a48fb2764b4f68b4d3d2b4
Miroslav Tzarev <mtzarev@gmail.com>
2024-02-06 18:37:33 +0200

    Add files via upload

---
+++ 3.Points Validation.js
@@ -0,0 +1,37 @@
+function pointsValidation(arr) {
+    let x1 = arr[0];
+    let x2 = arr[2];
+    let y1 = arr[1];
+    let y2 = arr[3];
+    let symbol = " "
+    function distanceX1Y1(x1, y1) {
+        if (Math.trunc(Math.sqrt(Math.pow(x1, 2) + Math.pow(y1, 2))) === Math.sqrt(Math.pow(x1, 2) + Math.pow(y1, 2))) {
+            return `{${x1}, ${y1}} to {0, 0} is valid`;
+        } else {
+            return `{${x1}, ${y1}} to {0, 0} is invalid`;
+        }
+    }
+    function distanceX2Y2(x2, y2) {
+        if (Math.trunc(Math.sqrt(Math.pow(x2, 2) + Math.pow(y2, 2))) === Math.sqrt(Math.pow(x2, 2) + Math.pow(y2, 2))) {
+            return `{${x2}, ${y2}} to {0, 0} is valid`;
+        } else {
+            return `{${x2}, ${y2}} to {0, 0} is invalid`;
+        }
+    }
+    function distanceX1Y2ToX2Y2(x1, y1, x2, y2) {
+        if (Math.trunc(Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2))) === Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2))) {
+            return `{${x1}, ${ y1}} to {${x2},${symbol}${y2}} is valid`;
+        } else {
+            return `{${x1}, ${ y1}} to {${x2},${symbol}${y2}} is invalid`;
+        }
+
+
+    }
+    console.log(distanceX1Y1(x1, y1));
+    console.log(distanceX2Y2(x2, y2));
+    console.log(distanceX1Y2ToX2Y2(x1, y1, x2, y2));
+}
+
+
+pointsValidation([3, 0, 0, 4]);
+//pointsValidation([2, 1, 1, 1]);
